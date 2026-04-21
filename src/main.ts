75e7627127fc50777fecebbae4ba9119a62599e5
ciandre <a.stanisic.codealkonost@gmail.com>
2026-04-21 20:06:20 +0200

    removing some files

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,6 @@
 import "./style.css";
+
 import { init } from "./init";
-import Dog from "./dog";
-import { Car } from "./car";
-
-const myDog = new Dog("Rex");
-myDog.bark();
-
-const myCar = new Car("Toyota", "Camry", 2020);
-console.log(`${myCar.year} ${myCar.make} ${myCar.model}`);
 
 init().catch((error) => {
   console.error("Greška prilikom inicijalizacije WebGPU-a:", error);
