489feb4edbfbaef72f5b3705f0451f24462b057d
ciandre <a.stanisic.codealkonost@gmail.com>
2026-04-21 19:50:50 +0200

    add class car to render car

---
+++ src/car.ts
@@ -0,0 +1,11 @@
+export class Car {
+  private make: string;
+  private model: string;
+  private year: number;
+
+  constructor(make: string, model: string, year: number) {
+    this.make = make;
+    this.model = model;
+    this.year = year;
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import "./style.css";
 import { init } from "./init";
 import Dog from "./dog";
+import Car from "./car";
 
 const myDog = new Dog("Rex");
 myDog.bark();
+
+const myCar = new Car("Toyota", "Camry", 2020);
+console.log(`${myCar.year} ${myCar.make} ${myCar.model}`);
 
 init().catch((error) => {
   console.error("Greška prilikom inicijalizacije WebGPU-a:", error);
