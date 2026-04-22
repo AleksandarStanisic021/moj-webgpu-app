215a6ee46806c9736150ace922ad8ad7e5fb0263
ciandre <a.stanisic.codealkonost@gmail.com>
2026-04-22 18:30:07 +0200

    fixes

--- src/main.ts
+++ src/main.ts
@@ -7,4 +7,12 @@ init().catch((error) => {
 });
 
 const { device, context, format, adapter } = await init();
+
+device.lost.then((event) => {
+  console.error("WebGPU uređaj je izgubljen:", event);
+  init().catch((error) => {
+    console.error("Greška prilikom ponovne inicijalizacije WebGPU-a:", error);
+  });
+});
+
 console.log(device, context, format, adapter);
