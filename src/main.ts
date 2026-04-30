d68d70e668f3e4dbf98b7ee019e0b065009223c3
ciandre <a.stanisic.codealkonost@gmail.com>
2026-04-30 22:47:53 +0200

    playing with colors

--- src/main.ts
+++ src/main.ts
@@ -50,7 +50,7 @@ async function Run() {
     colorAttachments: [
       {
         view: textureView,
-        clearValue: { r: 0.0, g: 0.0, b: 0.0, a: 1.0 },
+        clearValue: { r: 0.6, g: 1.0, b: 0.5, a: 1.0 },
         loadOp: "clear",
         storeOp: "store",
       },
